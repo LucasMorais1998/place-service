bce61fc5fc6aa33219f8b667f212231a603408f3
Lucas Morais <lucas14.morais@gmail.com>
2023-12-13 23:24:08 -0300

    feat(web): add PlaceController

---
+++ src/main/java/com/lucas/placeservice/web/PlaceController.java
@@ -0,0 +1,28 @@
+package com.lucas.placeservice.web;
+
+import com.lucas.placeservice.api.PlaceRequest;
+import com.lucas.placeservice.api.PlaceResponse;
+import com.lucas.placeservice.domain.PlaceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+    private PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Mono<PlaceResponse>> create(@RequestBody PlaceRequest request) {
+        var placeResponse = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
+    }
+}
