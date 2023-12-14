3b33faa92dc1b15e2c67694cf742f9c17514c544
Lucas Morais <lucas14.morais@gmail.com>
2023-12-13 23:24:51 -0300

    feat(config): add PlaceConfig

---
+++ src/main/java/com/lucas/placeservice/config/PlaceConfig.java
@@ -0,0 +1,15 @@
+package com.lucas.placeservice.config;
+
+import com.lucas.placeservice.domain.PlaceRepository;
+import com.lucas.placeservice.domain.PlaceService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PlaceConfig {
+
+    @Bean
+    PlaceService placeService(PlaceRepository placeRepository) {
+        return new PlaceService(placeRepository);
+    }
+}
