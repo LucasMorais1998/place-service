65ba5ddd2d4ffc32a531fa7e844bfbda8389aef2
Lucas Morais <lucas14.morais@gmail.com>
2023-12-14 22:07:21 -0300

    refactor(domain): add slugfy for automated slug generation in Place creation

--- src/main/java/com/lucas/placeservice/domain/PlaceService.java
+++ src/main/java/com/lucas/placeservice/domain/PlaceService.java
@@ -1,20 +1,23 @@
 package com.lucas.placeservice.domain;
 
+import com.github.slugify.Slugify;
 import com.lucas.placeservice.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
         var place = new Place(
                 null,
                 placeRequest.name(),
-                placeRequest.slug(),
+                slg.slugify(placeRequest.name()),
                 placeRequest.state(),
                 placeRequest.createdAt(),
                 placeRequest.createdAt());
