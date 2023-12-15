315c966468c4add7105fac3d1a41f09b017c3bf0
Lucas Morais <lucas14.morais@gmail.com>
2023-12-14 21:52:07 -0300

    feat(audit): add R2DBC auditing for Place

--- src/main/java/com/lucas/placeservice/config/PlaceConfig.java
+++ src/main/java/com/lucas/placeservice/config/PlaceConfig.java
@@ -4,8 +4,10 @@ import com.lucas.placeservice.domain.PlaceRepository;
 import com.lucas.placeservice.domain.PlaceService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
 
     @Bean
